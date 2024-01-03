fc187d5aeacef6460178659c199338316865d0e8
wiktoriahet <wiktoriaspyc@gmail.com>
2024-01-03 12:54:08 +0100

    created xml and xsd files, created StAX parser class to parse the xml file

---
+++ src/main/java/pl/hetman/wiktoria/solvd/StaxMain.java
@@ -0,0 +1,24 @@
+package pl.hetman.wiktoria.solvd;
+
+import pl.hetman.wiktoria.solvd.parser.ToyShopStaxParser;
+
+import javax.xml.stream.XMLStreamException;
+import java.io.FileNotFoundException;
+import java.nio.file.Paths;
+
+public class StaxMain {
+
+    public static void main(String[] args) {
+
+        String fileName = "src/main/resources/toyshop.xml";
+
+        ToyShopStaxParser toyShopStaxParser = new ToyShopStaxParser();
+        try {
+            toyShopStaxParser.printXmlByXmlCursorReader(Paths.get(fileName));
+        } catch (FileNotFoundException | XMLStreamException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+}
