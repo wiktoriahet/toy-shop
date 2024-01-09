e2b4c0be6986cc9cbb393d67751475ddefbf2c25
wiktoriahet <wiktoriaspyc@gmail.com>
2024-01-09 10:50:07 +0100

    myBatis homework small fix up

--- src/test/java/pl/hetman/wiktoria/solvd/service/impl/EmployeeEmailServiceImplTest.java
+++ src/test/java/pl/hetman/wiktoria/solvd/service/impl/EmployeeEmailServiceImplTest.java
@@ -12,6 +12,9 @@ import java.util.Optional;
 
 class EmployeeEmailServiceImplTest {
 
+    static {
+        System.setProperty("log4j.configurationFile", "log4j2.xml");
+    }
     @Test
     void checkFindById() throws ToyShopException {
         //given
@@ -21,9 +24,7 @@ class EmployeeEmailServiceImplTest {
         //when
         Optional<EmployeeContactEmail> foundedEmployeeEmail = employeeEmailService.findById(10L);
         List<Email> emails = foundedEmployeeEmail.orElse(null).getEmails();
-        Email email = emails.get(0);
-        String emailName = email.getEmail();
-        System.out.println(emailName);
+        System.out.println(foundedEmployeeEmail);
 
         //then
         Assertions.assertNotNull(foundedEmployeeEmail, "foundedEmployeeEmail is null");
