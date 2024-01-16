7acfda3969cc9f2f6037a73106e8d36d7998ebe2
wiktoriahet <wiktoriaspyc@gmail.com>
2024-01-16 18:03:36 +0100

    created facade

---
+++ src/main/java/pl/hetman/wiktoria/solvd/service/facade/ToyShopPromotionFacade.java
@@ -0,0 +1,21 @@
+package pl.hetman.wiktoria.solvd.service.facade;
+
+import pl.hetman.wiktoria.solvd.exceptions.OfferException;
+import pl.hetman.wiktoria.solvd.persistence.impl.DiscountRepositoryImpl;
+import pl.hetman.wiktoria.solvd.persistence.impl.ToyRepositoryMyBatisImpl;
+import pl.hetman.wiktoria.solvd.service.factory.OfferFactory;
+import pl.hetman.wiktoria.solvd.service.impl.DiscountServiceImpl;
+import pl.hetman.wiktoria.solvd.service.impl.ToyService;
+
+public class ToyShopPromotionFacade {
+
+    void choosePromotion(String option) throws OfferException {
+        DiscountRepositoryImpl discountRepository = new DiscountRepositoryImpl();
+        DiscountServiceImpl discountService = new DiscountServiceImpl(discountRepository);
+        ToyRepositoryMyBatisImpl toyRepositoryMyBatis = new ToyRepositoryMyBatisImpl();
+        ToyService toyService = new ToyService(toyRepositoryMyBatis);
+        OfferFactory offerFactory = new OfferFactory(discountService, toyService);
+        offerFactory.getOffer(option);
+    }
+
+}
